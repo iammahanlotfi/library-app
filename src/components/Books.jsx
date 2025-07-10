1f3324787194b6e374468a80478e600c231b01e4
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-07-10 12:54:41 +0430

    feat: Add Books and BookCard components to display books
    Books component handles the list of books, while BookCard displays individual book details like title, author, and cover.

---
+++ src/components/Books.jsx
@@ -0,0 +1,14 @@
+import {books} from "../constants/mockData.js"
+import BookCard from "./BookCard.jsx";
+function Books() {
+  return (
+    <div>
+        <div>{books.map((book) => (
+            <BookCard key={book.id}  data = {book} />
+        ))}</div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Books
